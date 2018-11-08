f0f8d2da42968d2391e48fdded98ef69cc030db9
Anderson Santos de Medeiros <anderson.santos.medeiros@hotmail.com>
2018-11-07 22:44:45 -0300

    Comentários adicionados nos scripts do player e inimigo, e agora o iminigo se move com metade da velocidade quando sob a luz da lanterna

--- Circo/Assets/Scripts/Enemy.cs
+++ Circo/Assets/Scripts/Enemy.cs
@@ -3,64 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+    // Velocidade de movimento do inimigo;
     [SerializeField]
     private float speed = 1;
+    // Tempo para que o inimigo volte a virar uma sombra depois que é transformado em objeto;
     [SerializeField]
     private int timeToShadowBack = 10;
+    // Tempo que o inimigo tem que passar debaixo da lanterna para virar um objeto;
     [SerializeField]
     private int timeToFadeShadow = 3;
 
     private SpriteRenderer sr;
     private GameObject player;
+    // Essa variável é usada para saber se o inimigo pode perseguir o player
     private bool canFollowPlayer;
+    // Variável para saber se o inimigo está debaixo da luz
     private bool underLight = false;
+    // Variável utilizada dentro da função SetUnderLight, para que uma parte da função só seja executada uma vez
     private bool firstTimeLighting = false;
+    // Variável responsável por dizer se o inimigo pode se mover ou não, uma vez que quando ele está transformado em objeto ele não pode se mover
     private bool canMove = true;
 
-	// Use this for initialization
-	void Start () {
+    // Aqui o sprite renderer é pegado para que o objeto mude o sprite quando passa x tempo debaixo da luz
+    void Start () {
         sr = GetComponent<SpriteRenderer>();
 	}
 	
 	void FixedUpdate () {
-        if(canFollowPlayer && !underLight && canMove)
-          transform.position = Vector2.Lerp(transform.position, player.transform.position, speed * Time.deltaTime);
+        // Verifica se pode se mover, se sim, persiga o jogador
+        if (canFollowPlayer && canMove) {
+            float movingSpeed = speed * Time.deltaTime;
+            if (underLight)
+                movingSpeed /= 2;
+            transform.position = Vector2.Lerp(transform.position, player.transform.position, movingSpeed);
+        }
     }
 
+    // Essa função é chamada de dentro do script do player, uma vez que a luz da lanterna esteja sobre o inimigo.
+    // Lembrando que ela é chamada várias vezes enquanto a lanterna iluminar o inimigo, por isso a necessidade de uma parte do código só ser executada uma vez.
     public void SetUnderLight() {
         underLight = true;
+        // Essa parte função só pode ser executada uma vez, já que ela possui o método que invoca uma outra função em um determinado tempo x
         if (!firstTimeLighting) {
             firstTimeLighting = true;
+            // Chama a função VerifyStatus após um tempo x em segundos, que é definido na variável timeToFadeShadow
             Invoke("VerifyStatus", timeToFadeShadow);
         }
     }
 
+    // Essa função é chamada para verificar se o inimigo ainda está sob a luz da lanterna após x segundos.
     private void VerifyStatus() {
         if (underLight) {
+    // Se estiver sob a luz da lanterna, então ele se transforma no objeto, parando de se mover até que a função TurnShadowOn seja chamado após y segundos, que é definido na variável timeToShadowBack
             canMove = false;
             sr.color = Color.white;
             Invoke("TurnShadowOn", timeToShadowBack);
         }
+        // Agora a variável que armazena se é a primeira vez que a luz bate no inimigo é resetada
         firstTimeLighting = false;
     }
 
+    // Essa função só é responsável por definir que o inimigo virou uma sombra novamente, podendo perseguir o player quando for o caso
     private void TurnShadowOn() {
         sr.color = Color.black;
         canMove = true;
     }
 
+    // Essa variável é chamada quando o player desliga a lanterna ou muda o direcionamento da mesma
     public void NotUnderLight() {
         underLight = false;
     }
 
+    // Essa função só verifica se o player entrou no raio do inimigo, que é definido por um colisor em volta do mesmo.
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             canFollowPlayer = true;
             player = collision.gameObject;
         }else if(collision.gameObject.tag == "Light") {
+            // Essa parte é importante pois, quando o inimigo entra em contato com o colisor que fica ao redor da luz própria do jogador, o inimigo deverá parar seu movimento.
             canFollowPlayer = false;
         }
     }
+    // Essa opção diz respeito a quando o player sai do raio do inimigo.
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
             canFollowPlayer = false;
--- Circo/Assets/Scripts/Player.cs
+++ Circo/Assets/Scripts/Player.cs
@@ -14,10 +14,14 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private float flashLightDistance = 100;
 
+    // Define o botão que liga a luz
+    const KeyCode TURNLIGHT = KeyCode.Space;
+
     //Para onde o jogador está se movendo
     //0 - baixo, 1 - direita, 2 - cima, 3 - esqueda
     private int moving = 0;
     private bool flashLightOn = false;
+    // Armazena se o botão para ligar a luz foi pressionado
     private bool turnOnLightPressed = false;
     private Animator animator;
     private Vector2 direction;
@@ -30,6 +34,7 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     private void Update () {
+        // Toda vez a luz é apagada e reacendida, uma vez que são 4 luzes no ambiente e o jogo precisa saber qual delas acender
         if (flashLightOn) {
             TurnOffFlashlight();
             TurnOnFlashlight();
@@ -37,6 +42,7 @@ public class Player : MonoBehaviour {
             TurnOffFlashlight();
         }
         GetInput();
+        // Verifica qual das luzes está ligada e traça um raio a partir dela, através da função VerifyHit
         if (flashlightDown.activeSelf) {
             VerifyHit(Vector2.down, transform);
         }else if (flashlightUp.activeSelf) {
@@ -50,25 +56,32 @@ public class Player : MonoBehaviour {
         }
 	}
 
+    // Essa função é responsável por traçar um raio de um ponto de origem até uma direção, verificando se há um inimigo no caminho
     private void VerifyHit(Vector2 direction, Transform origin) {
+        // Crie o 'raio' do ponto de origem em uma direção passada pelo script, com uma distância definida em flashLightDistance, colidindo somente na camada "Enemy", fazendo com que, se houver um inimigo no caminho, a retorne uma colisão
         RaycastHit2D hit = Physics2D.Raycast(origin.position, direction, flashLightDistance, LayerMask.GetMask("Enemy"));
+        // Se existir a colisão então o inimigo está sob a luz da lanterna
         if (hit.collider != null) {
-            hit.collider.gameObject.GetComponentInParent<Enemy>().SetUnderLight();
             enemy = hit.collider.gameObject;
+            // Chama a função SetUnderLight que está dentro do script do inimigo em que houve a colisão
+            enemy.GetComponentInParent<Enemy>().SetUnderLight();
         }
     }
 
+    // Função para desligar as luzes
     private void TurnOffFlashlight() {
         flashlightDown.SetActive(false);
         flashlightUp.SetActive(false);
         flashlightLeft.SetActive(false);
         flashlightRight.SetActive(false);
-        if(enemy != null)
+        // Se existia um inimigo sob a luz, então chame a função dele que define que o inimigo não está mais sob a luz
+        if (enemy != null)
             enemy.GetComponentInParent<Enemy>().NotUnderLight();
         enemy = null;
         flashLightOn = false;
     }
 
+    // Função que liga uma das 4 luzes de acordo para onde o jogador está olhando
     private void TurnOnFlashlight() {
         switch (moving) {
             case 0:
@@ -96,6 +109,7 @@ public class Player : MonoBehaviour {
         if (flashLightOn) {
             float x = transform.position.x;
             float y = transform.position.y;
+            // As 4 lanternas se movimentam de acordo com a posição do personagem na tela
             flashlightDown.transform.position = new Vector3(x, y, flashlightDown.transform.position.z);
             flashlightUp.transform.position = new Vector3(x, y, flashlightUp.transform.position.z);
             flashlightLeft.transform.position = new Vector3(x, y, flashlightLeft.transform.position.z);
@@ -111,7 +125,7 @@ public class Player : MonoBehaviour {
     private void GetInput () {
         direction = Vector2.zero;
 
-        if (Input.GetKeyDown(KeyCode.Space) && !turnOnLightPressed) {
+        if (Input.GetKeyDown(TURNLIGHT) && !turnOnLightPressed) {
             turnOnLightPressed = true;
             if (flashLightOn)
                 TurnOffFlashlight();
@@ -119,7 +133,7 @@ public class Player : MonoBehaviour {
                 TurnOnFlashlight();
         }
 
-        if (Input.GetKeyUp(KeyCode.Space)) {
+        if (Input.GetKeyUp(TURNLIGHT)) {
             turnOnLightPressed = false;
         }
 
