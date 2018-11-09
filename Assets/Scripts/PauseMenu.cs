4dd3e856e71abe40625c05d1c224200989cf9e54
Carlos Eduardo Alves Sarmento <edusrmt@imd.local>
2018-11-09 15:25:50 -0300

    commit inicial do menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+    GameObject container;
+
+    private void Start()
+    {
+        container = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (container.activeSelf)
+                Resume();
+            else
+                Pause();
+        }            
+	}
+
+    public void LoadMenu ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    void Pause ()
+    {
+        container.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    void Resume ()
+    {
+        container.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
